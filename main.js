978fed31b77dc89a549a72b45b04fb8290c3cbff
Sara000123 <sara1hoiom@gmail.com>
2022-09-20 02:24:18 +0200

    added image

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Frozen',
+            image: './assets/images/FR.jpg'
         }
     }
 })
